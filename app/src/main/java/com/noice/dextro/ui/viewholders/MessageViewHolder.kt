b08ede9f2fc2ebf05518a71aabc676a87fe16539
CmdDaniel <cmd.princedaniel@gmail.com>
2022-07-20 22:11:40 +0530

    - exploring custom theming

--- app/src/main/java/com/noice/dextro/ui/viewholders/MessageViewHolder.kt
+++ app/src/main/java/com/noice/dextro/ui/viewholders/MessageViewHolder.kt
@@ -1,4 +1,11 @@
 package com.noice.dextro.ui.viewholders
 
-class MessageViewHolder() {
+import androidx.recyclerview.widget.RecyclerView
+import com.noice.dextro.databinding.UserItemLayoutBinding
+
+class MessageViewHolder(private val bind: UserItemLayoutBinding):RecyclerView.ViewHolder(bind.root) {
+
+    fun bind(){
+        
+    }
 }
